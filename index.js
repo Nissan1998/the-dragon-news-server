7d0ee70d41167950094d5cb96f635f15f8b1baa8
Nissan1998 <jnnissanm@gmail.com>
2023-05-01 13:15:51 +0600

    dragonServerReadyToDelever The news

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require("express");
-const categories = require("./data/categories.json");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("dragon is coming");
 });
-app.use(cors());
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  if (parseInt(id) === 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+    console.log(id);
+  }
+});
+
 app.get("/categories", (req, res) => {
   res.send(categories);
 });
